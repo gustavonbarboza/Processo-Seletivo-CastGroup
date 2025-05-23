9bfbfb633a08841640b5b3cae6afdad8b70c16ca
Gustavo Barboza <gustabarboza37@gmail.com>
2025-05-23 00:36:49 -0300

    Criação do Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,61 @@
-import { Container } from './styles';
+import {   FooterContainer, FooterContent, Logo, SiteMap, FooterColuna, FooterTitle, FooterLink, SocialIcons } from './styles';
+import ImgLogo from '../../assets/images/logo/web-logo-cassi (white).svg';
+import { FaFacebookF, FaInstagram, FaLinkedin } from 'react-icons/fa';
 
 function Footer() {
   return (
-    <Container>
-      <p>© {new Date().getFullYear()} Empresa. Todos os direitos reservados.</p>
-    </Container>
+    <FooterContainer>
+      <FooterContent>
+        <Logo>
+          <img src={ImgLogo} alt="Logo Cassi" />
+          <SocialIcons>
+            <FaFacebookF />
+            <FaInstagram />
+            <FaLinkedin />
+          </SocialIcons>
+        </Logo>
+
+        <div className="divider" />
+
+        <SiteMap>
+          <FooterColuna>
+            <FooterTitle>Home</FooterTitle>
+          </FooterColuna>
+
+          <FooterColuna>
+            <FooterTitle>Conheça a CASSI</FooterTitle>
+            <FooterLink href="#">Institucional</FooterLink>
+            <FooterLink href="#">Informações financeiras</FooterLink>
+            <FooterLink href="#">Governança corporativa</FooterLink>
+            <FooterLink href="#">Como trabalhamos</FooterLink>
+            <FooterLink href="#">Faça parte da nossa equipe</FooterLink>
+          </FooterColuna>
+
+          <FooterColuna>
+            <FooterTitle>Nossos Planos</FooterTitle>
+            <FooterLink href="#">Plano CASSI Essencial</FooterLink>
+            <FooterLink href="#">Plano CASSI Família</FooterLink>
+            <FooterLink href="#">Plano CASSI Vida</FooterLink>
+            <FooterLink href="#">Quero contratar</FooterLink>
+            <FooterLink href="#">Plano de Associados</FooterLink>
+          </FooterColuna>
+
+          <FooterColuna>
+            <FooterTitle>Já tenho um plano CASSI</FooterTitle>
+            <FooterLink href="#">Cobertura do meu plano</FooterLink>
+            <FooterLink href="#">Cronogramas</FooterLink>
+            <FooterLink href="#">Telemedicina</FooterLink>
+          </FooterColuna>
+
+          <FooterColuna>
+            <FooterTitle>CliniCASSI</FooterTitle>
+            <FooterLink href="#">Atenção Primária Saúde</FooterLink>
+            <FooterLink href="#">Localize sua CliniCASSI</FooterLink>
+          </FooterColuna>
+
+        </SiteMap>
+      </FooterContent>
+    </FooterContainer>
   );
 }
 
--- src/components/Footer/styles.jsx
+++ src/components/Footer/styles.jsx
@@ -1,9 +1,81 @@
 import styled from 'styled-components';
 
-export const Container = styled.footer`
-  background-color: #282c34;
-  padding: 1rem;
+export const FooterContainer = styled.footer`
+  background-color: #002D4B;
   color: white;
-  text-align: center;
-  margin-top: auto;
+  height: 375;
+  padding-top: 40px;
+  padding-right: 80px;
+  padding-bottom: 40px;
+  padding-left: 80px;
+  gap: 32px;
+`;
+
+export const FooterContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+
+  .divider {
+    width: 100%;
+    border-bottom: 1px solid #FCFC1B;
+    margin-bottom: 16px;
+  }
+`;
+
+export const Logo = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  img {
+    height: 50px;
+  }
+`;
+
+export const SocialIcons = styled.div`
+  display: flex;
+  gap: 16px;
+
+  svg {
+    color: white;
+    font-size: 20px;
+    cursor: pointer;
+  }
+`;
+
+export const SiteMap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  gap: 24px;
+`;
+
+export const FooterColuna = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  min-width: 160px;
+`;
+
+export const FooterTitle = styled.h4`
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 28px;
+  letter-spacing: 0%;
+  vertical-align: middle;
+  text-decoration: underline;
+  text-decoration-style: solid;
+  text-decoration-thickness: 0%;
+`;
+
+export const FooterLink = styled.a`
+  font-size: 14px;
+  text-decoration: none;
+  color: white;
+  opacity: 0.85;
+  font-weight: 400;
+  line-height: 22px;
+  letter-spacing: 0%;
+  vertical-align: middle;
 `;
